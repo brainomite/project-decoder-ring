88ac8c4c7a23ac937759e6957bc4fdc00ca52a0f
Aaron Young <hi@aaronyoung.dev>
2020-12-22 12:36:26 -0500

    style: Apply prettier to caesar

--- src/caesar.js
+++ src/caesar.js
@@ -9,24 +9,26 @@ function caesar(input, shift, encode = true) {
   const actualShift = encode ? shift : shift * -1;
 
   // translate each character and return the new string
-  return input
-    // treat all characters as lowercase
-    .toLowerCase()
-    .split("")
-    .map((letter) => {
-      // find the position of the letter
-      const origIndex = ALPHABET_ARR.indexOf(letter);
-      // if its not alphabetic, return the original letter, intact
-      if (notAlphabetic(origIndex)) return letter;
-      // math trick, use the remainder to auto-wrap around the array
-      const newIndex = (origIndex + actualShift) % 26;
-      return newIndex >= 0
-        ? // approach from the left as index is not negative
-          ALPHABET_ARR[newIndex]
-        : // newIndex is negative here, so we approach from the right
-          ALPHABET_ARR[26 + newIndex];
-    })
-    .join("");
+  return (
+    input
+      // treat all characters as lowercase
+      .toLowerCase()
+      .split("")
+      .map((letter) => {
+        // find the position of the letter
+        const origIndex = ALPHABET_ARR.indexOf(letter);
+        // if its not alphabetic, return the original letter, intact
+        if (notAlphabetic(origIndex)) return letter;
+        // math trick, use the remainder to auto-wrap around the array
+        const newIndex = (origIndex + actualShift) % 26;
+        return newIndex >= 0
+          ? // approach from the left as index is not negative
+            ALPHABET_ARR[newIndex]
+          : // newIndex is negative here, so we approach from the right
+            ALPHABET_ARR[26 + newIndex];
+      })
+      .join("")
+  );
 }
 
 module.exports = caesar;
